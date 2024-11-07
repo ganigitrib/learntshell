b849a513da4813b7e1ddf6219ffdafa3d79b6af9
ganeshkaibada1 <ganeshkaibada7036@gamil.com>
2024-11-07 20:32:49 +0530

    print script

--- 02-print.sh
+++ 02-print.sh
@@ -1,1 +1,8 @@
 echo hello world
+
+echo -e "\e[31hello world in Red color\e[0m"
+echo -e "\e[32hello world in Green color\e[0m"
+echo -e "\e[33hello world in Yellow color\e[0m"
+echo -e "\e[34hello world in Blue color\e[0m"
+echo -e "\e[35hello world in Magenta color\e[0m"
+echo -e "\e[36hello world in Cyan color\e[0m"
